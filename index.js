4ce2a264ffeedb2540145a98505495a2a1ce98e7
devmdtanvir <officialtanvir@hotmail.com>
2023-02-17 12:23:02 +0600

    I am added all file

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 document.getElementById('calculate').addEventListener('click', (e) => {
     e.preventDefault()
-    const foodValue = document.getElementById('food').value
-    const rentValue = document.getElementById('rent').value
-    const clothesValue = document.getElementById('clothes').value
-    const food = parseInt(foodValue)
-    const rent = parseInt(rentValue)
-    const clothes = parseInt(clothesValue)
-    const totalExpness = food + rent + clothes
-    console.log('totalExpness', totalExpness)
+    const food = valueIdentifiy('food')
+    const rent = valueIdentifiy('rent')
+    const clothes = valueIdentifiy('clothes')
+    const income = valueIdentifiy('income')
+    const totalExpness = Expness(food, rent, clothes)
+    const balance = income - totalExpness
+    document.getElementById('balance').innerText = balance
 })
+const valueIdentifiy = (id) => {
+    const value = parseInt(document.getElementById(id).value)
+    return value
+}
+
+document.getElementById('save-button').addEventListener('click', () => {
+    const incomeValue = document.getElementById('income').value
+    const income = parseInt(incomeValue)
+    const saveValue = document.getElementById('save').value
+    const savePercent = saveValue / 100
+    const totalSave = income * savePercent
+    document.getElementById('saving').innerText = totalSave
+    console.log(totalSave)
+})
+
+const Expness = (food, rent, clothes) => {
+    const totalExpness = food + rent + clothes
+    document.getElementById('expanse').innerText = totalExpness
+    return totalExpness
+}
